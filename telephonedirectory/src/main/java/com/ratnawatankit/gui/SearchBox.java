ae2012e117b3f22cb5ff7e3a3099900bbfa56321
Ankit Ratnawat <ratnawat.ankit1@gmail.com>
2020-05-09 22:59:00 +0530

    Update file to see searched object data on UI

--- telephonedirectory/src/main/java/com/ratnawatankit/gui/SearchBox.java
+++ telephonedirectory/src/main/java/com/ratnawatankit/gui/SearchBox.java
@@ -5,6 +5,8 @@ import com.ratnawatankit.service.TelPhoneDirectory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.List;
@@ -13,7 +15,7 @@ public class SearchBox extends JPanel {
     private final JTextField tf;
     private final JComboBox combo = new JComboBox();
 
-    public SearchBox(TelPhoneDirectory trie) {
+    public SearchBox(TelPhoneDirectory trie, JTextField jtfName, JTextField jtfPhone, JTextField jtfEmail, JTextField jtfCompany) {
         super(new BorderLayout());
         combo.setEditable(true);
         tf = (JTextField) combo.getEditor().getEditorComponent();
@@ -38,24 +40,46 @@ public class SearchBox extends JPanel {
                 });
             }
         });
+
+        combo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox cBox = (JComboBox) e.getSource();
+                Directory selectedItem = (Directory) cBox.getSelectedItem();
+                if (selectedItem != null) {
+                    jtfName.setText(selectedItem.getFullName());
+                    jtfCompany.setText(selectedItem.getCompanyName());
+                    jtfPhone.setText(selectedItem.getPhone());
+                    jtfEmail.setText(selectedItem.getEmail());
+                } else {
+                    jtfName.setText(null);
+                    jtfCompany.setText(null);
+                    jtfPhone.setText(null);
+                    jtfEmail.setText(null);
+                }
+            }
+        });
+
         setModel(new DefaultComboBoxModel(), "");
         JPanel p = new JPanel(new BorderLayout());
-        p.setBorder(BorderFactory.createTitledBorder("AutoSuggestion Box"));
+        p.setBorder(BorderFactory.createTitledBorder("Select Contact to check details"));
         p.add(combo, BorderLayout.NORTH);
         add(p);
         setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setPreferredSize(new Dimension(300, 150));
     }
+
     private void setModel(DefaultComboBoxModel mdl, String str) {
         combo.setModel(mdl);
         combo.setSelectedIndex(-1);
         tf.setText(str);
     }
+
     private static DefaultComboBoxModel getSuggestedModel(TelPhoneDirectory trie, String text) {
         DefaultComboBoxModel m = new DefaultComboBoxModel();
         List<Directory> names = trie.queryResults(text);
-        if (names!=null) {
-            names.stream().forEach(x->m.addElement(x));
+        if (names != null) {
+            names.stream().forEach(x -> m.addElement(x));
         }
         return m;
     }
