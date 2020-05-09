f2a29da401a3b335566f979d8d0025c7e5f5b2b1
Ankit Ratnawat <ratnawat.ankit1@gmail.com>
2020-05-09 23:04:19 +0530

    Added Constant value

--- telephonedirectory/src/main/java/com/ratnawatankit/utility/Constants.java
+++ telephonedirectory/src/main/java/com/ratnawatankit/utility/Constants.java
@@ -2,4 +2,6 @@ package com.ratnawatankit.utility;
 
 public class Constants {
     public static final String FRAME_TITLE = "Telephone Book";
+    public static final String DIRECTORY_PATH = "directory.csv";
+
 }
